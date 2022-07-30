0b19862d09c4ea0d1b99eb159c0097c82ceb4655
junior-slv <dirceusilva0422@gmail.com>
2022-07-30 13:17:54 -0300

    Presence list.

--- reactapp/src/pages/home/index.jsx
+++ reactapp/src/pages/home/index.jsx
@@ -1,15 +1,62 @@
-import './styles.css';
-import {Card} from '../../components/Card';
-export function Home(){
-    return(
-        <div className='container'>
-            <h1>Presence </h1>
-            <input type="text" placeholder="Insert name.." />
-            <button type="button">Add</button>
+import React, { useState, useEffect } from "react";
 
-            <Card name="Rodrigo" time="10:34:23"/>
-            <Card name='Joao' time='11:24:23'/>
-            
+import "./styles.css";
+import { Card } from "../../components/Card";
+
+export function Home() {
+  const [studentName, setStudentName] = useState("");
+  const [students, setStudents] = useState([]);
+  const [user,setUser] = useState({name: '', avatar:'' }); 
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-BR", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      }),
+    };
+
+    setStudents((prevState) => [...prevState, newStudent]);
+  }
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/junior-slv")
+      .then((response) => response.json())
+      .then((data) => {
+        setUser({
+          name: data.name,
+          avatar: data.avatar_url,
+        });
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <header>
+        <h1>Presence list</h1>
+        <div>
+            <strong>{user.name}</strong>
+            <img src={user.avatar} alt="profile_photo" />
         </div>
-    )
-} ;
+      </header>
+
+      <input
+        type="text"
+        placeholder="Insert name..."
+        onChange={(e) => setStudentName(e.target.value)}
+      />
+      <button type="button" onClick={handleAddStudent}>
+        Add
+      </button>
+
+      {students.map((student) => (
+        <Card 
+         key={student.time}
+         name={student.name} 
+         time={student.time} 
+        />
+      ))}
+    </div>
+  );
+}
